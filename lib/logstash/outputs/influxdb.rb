9a382d241e496379ad1005a66dfa0793528bc795
Anumodh Narayanan <anumodh@gmail.com>
2014-05-15 00:34:25 +0530

    commiting basic structure of influxDB output plugin

--- lib/logstash/outputs/influxdb.rb
+++ lib/logstash/outputs/influxdb.rb
@@ -6,6 +6,13 @@ class LogStash::Outputs::InfluxDB < LogStash::Outputs::Base
   config_name "influxdb"
   milestone 1
 
+  # username to connect to influxdb
+  config :username, :validate => :string, :required => :true
+  config :password, :validate => :string, :required => :true
+  config :database, :validate => :string, :required => :true
+  config :series_name, :validate => :string, :required => :true
+
+
   public
   def register
     require 'influxdb'
